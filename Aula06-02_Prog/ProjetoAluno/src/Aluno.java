6bfcd99867900775f3fc0ddbc23e75bfa7a53223
Daniel Tan <57137640+Datan24@users.noreply.github.com>
2020-03-03 09:29:34 -0300

    Add files via upload

---
+++ Aula06-02_Prog/ProjetoAluno/src/Aluno.java
@@ -0,0 +1,13 @@
+
+public class Aluno {
+
+	// Atributo ou Propriedades ou variavel de instancia.
+	int rm;
+	String nome;
+	
+	//metodo para imprimir os dados do aluno
+	public void imprimir() {
+		System.out.println("RM --> "+rm);
+		System.out.println("RM --> "+nome);
+	}
+}
---
+++ Aula06-02_Prog/ProjetoAluno/src/Principal.java
@@ -0,0 +1,10 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// declaracao de variavel --- instanciacao
+		Aluno a = new Aluno();
+		a.imprimir();
+	}
+
+}
